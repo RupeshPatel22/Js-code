79f48951598d4e81952329f1cfe7d7a957631569
RupeshPatel22 <114584498+RupeshPatel22@users.noreply.github.com>
2024-11-12 01:33:16 +0000

    basics of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14325;
+let accountEmail = "rupeshpatel@js.com";
+var accountPassword = '12345';
+accountCity = "Surat";
+let accountState;
+
+// accountId = 21;
+
+accountEmail = "rp@js.com";
+accountPassword = '212121212';
+accountCity = "Mumbai";
+
+console.table([accountId, accountEmail,accountPassword, accountCity,accountState]);
+
+// console.log(accountId);
