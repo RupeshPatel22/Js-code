88d1f6072886880e311db735e5ee766db9d424c6
RupeshPatel22 <114584498+RupeshPatel22@users.noreply.github.com>
2024-11-13 01:37:30 +0000

    Data types

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,12 +4,16 @@ var accountPassword = '12345';
 accountCity = "Surat";
 let accountState;
 
-// accountId = 21;
+// accountId = 21; //Not allowed
 
 accountEmail = "rp@js.com";
 accountPassword = '212121212';
 accountCity = "Mumbai";
 
+/* 
+Prefer not to use var 
+because of the issue in block scope and functional scope 
+*/
 console.table([accountId, accountEmail,accountPassword, accountCity,accountState]);
 
 // console.log(accountId);
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,38 @@
+"use strict" //treat all the js code to newer version
+
+//alert(3+3); //we are using node, not browser
+
+console.log(3
+    +
+    3); //code readability 
+console.log("Rupesh");
+
+let name = "Rupesh"
+let age = 18
+let isLoggedIn = false
+let address = null
+let state
+
+//Primtive data types
+
+    //number => range (2 to power of 53)
+    //binint => more big number
+    //string =>""
+    //boolean => true/false
+    //null => standalone value/intentional absence of value
+    //undefined 
+    //symbol => unique
+
+// Non-primtive data types
+
+    //object
+    //array
+    //interface
+    //function
+
+console.log(typeof(undefined)) //data type of undefined is undefined
+console.log(typeof(null)) //data type of null is object
+
+
+
+
