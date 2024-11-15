cdd866f1f12767c1c4674d98a88e2164c1971104
RupeshPatel22 <114584498+RupeshPatel22@users.noreply.github.com>
2024-11-15 01:44:20 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+score = "true"
+
+// console.log(typeof score);
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33 convert in number easily
+// "33abc" => conver in number but value is NaN(not a number)
+// true => 1; false => 0
+
+let isLoggedIn = "df"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "string" = true
+
+let number = true
+
+let stringNumber = String(number);
+// console.log(typeof stringNumber);
+
+// *********************** operations ******************************************
+
+let value = 10
+let negValue = -value;
+
+// console.log(negValue);
+
+let str1 = "Rupesh"
+let str2 = "Patel"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+(2+2));
+// console.log(1+2+"2");
+// console.log(((3+4)*5)%10);
+
+// console.log(+true); //convert in number
+// console.log(+"");
+
+let num = 100;
+--num;
+console.log(num);
+
+
+
+
+
