408b93acf8f60fa6b5c8d784f004350cb239fd7e
RupeshPatel22 <114584498+RupeshPatel22@users.noreply.github.com>
2024-12-05 11:21:25 +0000

    filter

---
+++ 05_iterations/filter-map-reduce.js
@@ -0,0 +1,12 @@
+// const clubs = ["fcb","madrid","liverpool","united","city","bayern"]
+
+// const value = clubs.forEach((item) => {
+//     // console.log(item);
+//     return item
+// } )
+// console.log(value);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+const newNums = myNums.filter((item) => item > 4)
+console.log(newNums);
